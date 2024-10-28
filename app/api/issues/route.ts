4bfd1653822c8d7be49374794410e0e865a3df65
Rakib Jahan <rakib@gameloops.io>
2024-10-28 15:51:20 +0600

    Build an API for creating issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  const newIssue = await prisma.issue.create({
+    data: { name: body.name, dsecription: body.description },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
