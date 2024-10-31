1d9afc1d195c1106fac762d5f64d317a9ccc7c06
Rakib Jahan <rakib@gameloops.io>
2024-10-31 14:34:40 +0600

    Edit issue button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import Markdown from "react-markdown";
-
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 interface Props {
   params: { id: string };
 }
@@ -15,16 +16,28 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading>{issue.name}</Heading>
-      <Flex gap="3" py="3">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose">
-        <Markdown>{issue.dsecription}</Markdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="1rem">
+      <Box>
+        <Heading>{issue.name}</Heading>
+        <Flex gap="3" py="3">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose">
+          <Markdown>{issue.dsecription}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Link href={`/issues/${issue.id}/edit</Button>`}>
+            <Flex align="center" gap="2">
+              <Pencil1Icon />
+              <Text>Edit Issue</Text>
+            </Flex>
+          </Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
