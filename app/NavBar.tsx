228c635f2a5a1854ab36326daee011f8ea0be0a4
Rakib Jahan <rakib@gameloops.io>
2024-11-03 15:21:16 +0600

    Navbar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { FaBug } from "react-icons/fa";
 import classNames from "classnames";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -17,42 +18,46 @@ const NavBar = () => {
   classNames;
 
   return (
-    <nav className="flex space-x-6 h-14 items-center p-6 border-b mb-5">
-      <Link href={"/"}>
-        <FaBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": pathname == link.href,
-                "text-zinc-500 hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
+    <nav className="p-3 border-b mb-5">
+      <Flex justify="between">
+        <Flex align="center" gap="3">
+          <Link href={"/"}>
+            <FaBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classNames({
+                    "text-zinc-900": pathname == link.href,
+                    "text-zinc-500 hover:text-zinc-800 transition-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Box className="ml-auto flex space-x-4">
+          {session ? (
+            <button
+              onClick={() => signOut()}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
             >
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="ml-auto flex space-x-4">
-        {session ? (
-          <button
-            onClick={() => signOut()}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Sign Out
-          </button>
-        ) : (
-          <button
-            onClick={() => signIn("google")}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            Sign In
-          </button>
-        )}
-      </div>
+              Sign Out
+            </button>
+          ) : (
+            <button
+              onClick={() => signIn("google")}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              Sign In
+            </button>
+          )}
+        </Box>
+      </Flex>
     </nav>
   );
 };
