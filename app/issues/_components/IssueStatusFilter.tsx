6a6a1f4a6b731ca05d5d1b69d59432f8e61434f6
Rakib Jahan <rakib@gameloops.io>
2024-11-04 22:26:48 +0600

    2 params working at once

--- app/issues/_components/IssueStatusFilter.tsx
+++ app/issues/_components/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const IssueStatusFilter = () => {
@@ -13,13 +13,21 @@ const IssueStatusFilter = () => {
   ];
 
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <>
       <Select.Root
         onValueChange={(status) => {
-          const params = status == "None" ? "" : "?status=" + status;
-          router.push("/issues" + params);
+          const params = new URLSearchParams();
+          if (status) params.append("status", status);
+          if (searchParams.get("orderBy"))
+            params.append("orderBy", searchParams.get("orderBy")!);
+
+          const query = params.size ? "?" + params.toString() : "";
+          router.push("/issues" + query);
+          // const params = status == "None" ? "" : "=" + status;
+          // router.push("/issues?status" + params);
         }}
       >
         <Select.Trigger placeholder="Filter by..." />
