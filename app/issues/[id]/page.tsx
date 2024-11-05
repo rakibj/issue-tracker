2cf6f2065d755ad9c8dbfc2be702cf54d714f633
Rakib Jahan <rakib@gameloops.io>
2024-11-05 12:42:40 +0600

    optimize using cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,14 +6,20 @@ import IssueDetails from "./IssueDetails";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import SelectAssignee from "./SelectAssignee";
+import { cache } from "react";
 interface Props {
   params: Promise<{ id: string }>;
 }
+
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailsPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await params;
   const issueId = parseInt(id);
-  const issue = await prisma.issue.findUnique({ where: { id: issueId } });
+  const issue = await fetchIssue(issueId);
   if (!issue) notFound();
 
   return (
@@ -33,7 +39,7 @@ const IssueDetailsPage = async ({ params }: Props) => {
 
 export async function generateMetadata({ params }: Props) {
   const id = (await params).id;
-  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  const issue = await fetchIssue(parseInt(id));
   return {
     title: issue?.name,
     description: issue?.dsecription,
